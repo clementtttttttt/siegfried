16ecdf3176be87b620937e58725cb8e47a62aae4
clement <clementchpoon@gmail.com>
2023-06-01 09:41:04 +0100

    fixed ioapic regs not r/wing correctly

--- apic.c
+++ apic.c
@@ -6,16 +6,16 @@
 
 acpi_madt *madt;
 
-unsigned int* ioapic_addr;
+volatile unsigned int* ioapic_addr;
 
 void apic_write_reg(unsigned char idx, unsigned int val){
     ioapic_addr[0] = idx;
-    ioapic_addr[0x4] = val;
+    ioapic_addr[0x5] = val;
 }
 
 unsigned int apic_read_reg(unsigned char idx){
     ioapic_addr[0] = idx;
-    return ioapic_addr[0x4];
+    return ioapic_addr[4];
 }
 
 void apic_get_redir_ent(unsigned int irq, apic_redir_ent *ent){
@@ -47,13 +47,15 @@ void apic_setup(){
                 if(ce->int_base == 0){
                     //only deal with irqs
 
-                    ioapic_addr = page_map_paddr((unsigned long)ce, 1);
+                    ioapic_addr = page_map_paddr((unsigned long)ce->apic_addr, 1);
                     draw_string("IRQ IOAPIC VADDR ");
                     draw_hex((unsigned long)ioapic_addr);
+                    draw_string("IRQ IOAPIC PADDR ");
+                    draw_hex((unsigned long)ce->apic_addr);
 
                     apic_redir_ent ent;
                     apic_get_redir_ent(0, &ent);
-                    draw_hex(apic_read_reg(0));
+                    draw_hex(apic_read_reg(1));
                     draw_hex((unsigned long)ent.raw);
 
                 }
