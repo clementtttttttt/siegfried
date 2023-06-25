02d24518a08ac87f0bd8c8b93cb759359f247477
clement <clementchpoon@gmail.com>
2023-06-25 19:42:45 +0100

    add runner

---
+++ runner.c
@@ -0,0 +1,9 @@
+#include "runner.h"
+#include "klib.h"
+
+int runner_spawn_from_file_at_root(unsigned long disk_inode, char *path){
+
+
+
+    return 0;
+}
---
+++ runner.h
@@ -0,0 +1,1 @@
+int runner_spawn_from_file_at_root(unsigned long disk_inode, char *path);
