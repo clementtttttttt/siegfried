a14d1c50884af86d7c79808906abca2c922b6c4c
clement <clementchpoon@gmail.com>
2024-08-02 00:24:51 +0800

    move toolchain to different repo

--- toolchain/hagen/include/fcntl.h
+++
@@ -1,3 +0,0 @@
-#define O_RDONLY  00
-#define O_WRONLY  01
-#define O_RDWR    02
--- toolchain/hagen/include/sys/types.h
+++
@@ -1,4 +0,0 @@
-typedef long off_t;
-typedef __uint128_t sigset_t;
-typedef unsigned long pid_t;
-typedef long		ssize_t;
